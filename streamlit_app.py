a05837bdd32dc674985ca6bcd318ff02c0b735bb
ctlowberrie <jjustanya@gmail.com>
2026-05-22 14:21:51 +0700

    Fix formatting and add import statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("🎈 projek kelas A")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
 import streamlit as st
 
 st.markdown("*Streamlit* is **really** ***cool***.")
